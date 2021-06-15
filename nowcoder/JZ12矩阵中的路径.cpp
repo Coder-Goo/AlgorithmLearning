a61a46152b80e4940c6b4d09f570239fcee3d2ab
Coder-Goo <417648023@qq.com>
2021-06-15 22:58:31 +0800

    回溯

---
+++ nowcoder/JZ12矩阵中的路径.cpp
@@ -0,0 +1,28 @@
+
+//思路： 深度优先(回溯)
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        rows = board.size();
+        cols = board[0].size();
+        n = word.size();
+        for(int i = 0; i< rows; i++ ) {
+            for(int j = 0; j< cols; j++) {
+                if(DFS(board, word, i,j,0)) return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    int rows, cols, n;
+    bool DFS(vector<vector<char>>& board, string & word, int i,int j,int k) {
+        if(i>=rows || i<0 || j>=cols || j<0 ||k>= n|| board[i][j] != word[k]) return false;
+        if(k == n -1) return true;
+        board[i][j] = '\0';
+        bool ret = DFS(board, word, i+1,j,k+1) ||DFS(board, word, i-1,j,k+1) 
+        ||DFS(board, word, i,j+1,k+1)||DFS(board, word, i,j-1,k+1);
+        board[i][j] = word[k];
+        return ret;
+    }
+};
