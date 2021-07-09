ec98fc626d35e5a1c2718379517f3413c11d81bd
Coder-Goo <417648023@qq.com>
2021-07-09 11:32:28 +0800

    update

--- nowcoder/剑指offer/JZ42连续子数组的最大和.cpp
+++ nowcoder/剑指offer/JZ42连续子数组的最大和.cpp
@@ -7,13 +7,32 @@
 //     求最值
 //     遍历，以第i个元素结尾的数组的最大子数组的和，找出最大的，就是整个数组的最大值
 //     动态规划
-//     dp[i]定义为以第i个元素结尾的连续数组的最大和
+//     dp[i]定义为以下标为i个元素结尾的连续数组的最大和
 //     以第i个元素结尾这样的设定才好设计第i个元素nums[i]的转移方程
     
 //     转移方程： 如果dp[i-1]<= 0; 那么说明dp[i-1] 对dp[i]产生负贡献，即dp[i-1] + nums[i]
-//     还不如nums[i]本身大。那么nums[i]就自成一块，因为要满足连续要求
+//     还不如nums[i]本身大。那么nums[i]就自成一块，因为要满足连续要求; 
+//
+//则有如下转移方程：
 //         - 当dp[i-1]>0; 执行dp[i] = dp[i-1]+ nums[i];
 //         - 当dp[i-1]<=0; 执行dp[i] = nums[i] + 0;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 0) return 0;
+        vector<int>dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int result = dp[0];
+        for(int i= 1; i < nums.size(); ++i) {
+            if(dp[i-1] > 0) dp[i] = dp[i-1] + nums[i];
+            else dp[i] = nums[i];
+            result = max(result, dp[i]);
+        }
+        return result;
+    }
+};
+
 
 
 //动态规划,dp[i]表示第i个元素结尾的 最大连续子序列 和，那么我们就是求max(d[i])
@@ -41,38 +60,38 @@ public:
 
 //暴力解法;
 
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int result = INT_MIN;
-//         for(int i = 0; i< nums.size(); ++i) {
-//             int count = 0;
-//             for(int j = i ; j<nums.size(); ++j) {
-//                 count += nums[j];
-//                 result = result > count ? result : count;
-//             }
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = INT_MIN;
+        for(int i = 0; i< nums.size(); ++i) {
+            int count = 0;
+            for(int j = i ; j<nums.size(); ++j) {
+                count += nums[j];
+                result = result > count ? result : count;
+            }
 
-//         }
-//         return result;
-//      }
-// };
+        }
+        return result;
+     }
+};
 
 //贪心算法  ;因为加上负数只会减少总和，那么如果加上一个数后总和变为负值，那么总和从下一个数开始算起，这就是贪心的地方
-// class Solution {
-// public:
-//     int maxSubArray(vector<int>& nums) {
-//         int result = INT_MIN;
-//         int count = 0;
-//         for(int i =0 ; i< nums.size(); ++i) {
-//             count += nums[i];
-//             if(count > result) {
-//                 result = count;
-//             }
-//             if(count<0) {
-//                 count = 0;
-//             }
-//         }
-//         return result;
-//     }
-// };
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int result = INT_MIN;
+        int count = 0;
+        for(int i =0 ; i< nums.size(); ++i) {
+            count += nums[i];
+            if(count > result) {
+                result = count;
+            }
+            if(count<0) {
+                count = 0;
+            }
+        }
+        return result;
+    }
+};
 
